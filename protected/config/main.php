eafb40851cfbbcc00da8d1d7ad1d82674dfb27a1
123 <1234>
2014-10-15 18:11:37 +0200

    какой-нибудь текст

--- protected/config/main.php
+++ protected/config/main.php
@@ -55,7 +55,7 @@ return array(
           ), */
         // uncomment the following to use a MySQL database
         'db' => array(
-            'connectionString' => 'mysql:host=localhost;dbname=yii2',
+            'connectionString' => 'mysql:host=localhost;dbname=yii2;port=3307;',
             'emulatePrepare' => true,
             'username' => 'root',
             'password' => '',
