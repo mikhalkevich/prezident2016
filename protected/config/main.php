30f95a5a28c39dedd37fe571f9b4b3e41db41388
Fisgard <Fsgard@mail.ru>
2014-10-15 18:06:46 +0300

    del. newfile

--- protected/config/main.php
+++ protected/config/main.php
@@ -40,11 +40,8 @@ return array(
             ),
         ),
         'db' => array(
-<<<<<<< HEAD
             'connectionString' => 'mysql:host=localhost;dbname=yii2;port=3309',
-=======
-            'connectionString' => 'mysql:host=localhost;dbname=yii2;port=3302',
->>>>>>> remotes/origin/Neff
+
             'emulatePrepare' => true,
             'username' => 'root',
             'password' => '',
